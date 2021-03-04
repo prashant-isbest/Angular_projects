81cbd4728f59377398af2794b25ec0c9b42d9244
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-04 17:43:20 +0530

    using component styles and attribute component  selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/success/success.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './success.component.html',
+  selector: 'app-success',
+  styles: ['h3{color:green}'],
+})
+export class SuccessComponent {}
