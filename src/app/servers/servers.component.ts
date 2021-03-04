9155c947afac1770bba503e1fb03a56594a4f2c2
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-04 18:59:18 +0530

    using string interpolation and data binding in servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  allowNewServer = false;
+  constructor() {
+    setInterval(() => {
+      this.allowNewServer = !this.allowNewServer;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
 }
