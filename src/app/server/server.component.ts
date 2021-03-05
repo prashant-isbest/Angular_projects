d8c56109e8e8d9d1876d8ec8062631c7d4eec2fa
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-05 16:20:41 +0530

    using ngStyle attribute directive and propertybinding to configure it

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'lightgreen' : 'red';
+  }
+}
